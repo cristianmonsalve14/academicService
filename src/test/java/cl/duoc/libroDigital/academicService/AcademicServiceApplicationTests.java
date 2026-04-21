b5d5e9a7cb1b71d62300bfdc6234feeb47a6c4cb
cristianmonsalve14 <c.m.budrovich@gmail.com>
2026-04-20 20:31:37 -0400

    primer comit de repositorio del microservicio academicService

---
+++ src/test/java/cl/duoc/libroDigital/academicService/AcademicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.libroDigital.academicService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
